65b4d57f9693145a99bdabf217bd2fbe4d2f6982
Jöran Stark <joran.stark@uadm.uu.se>
2019-08-17 13:52:29 +0200

    Add snake.js

---
+++ snake.js/precache-manifest.c6b2f20fce3627bb3e80a7ee8174b5d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c489ee160012164fde83a4cc90dc22",
+    "url": "jorans.github.io/index.html"
+  },
+  {
+    "revision": "bb85d8b89d238e3616bc",
+    "url": "jorans.github.io/static/css/main.4627d054.chunk.css"
+  },
+  {
+    "revision": "b19e0e1ebfd1c1d7fe46",
+    "url": "jorans.github.io/static/js/2.f61800a5.chunk.js"
+  },
+  {
+    "revision": "bb85d8b89d238e3616bc",
+    "url": "jorans.github.io/static/js/main.4f984ce8.chunk.js"
+  },
+  {
+    "revision": "8a4232116eb81a5e8a38",
+    "url": "jorans.github.io/static/js/runtime~main.17587e5c.js"
+  }
+]);
